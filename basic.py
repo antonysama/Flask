8868edb2429e219ca7d6de85f485e9dcc63ee03c
antonysama <antonysama@gmail.com>
2020-09-27 01:39:01 -0600

    basic.py

--- basic.py
+++ basic.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello puppy!</h1>"
-    
+    return '<h1>Hello Puppy!</h1>'
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0") 
